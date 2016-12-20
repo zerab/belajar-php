2bf67bcec225b42d47ad8ed4594b706f40b78f03
Abihu Haezer <abihu.haezer@gmail.com>
2016-12-20 09:45:44 +0700

    Brand new belajar-php

---
+++ Day 12/Buah.php
@@ -0,0 +1,13 @@
+<?php
+
+class Buah
+{
+    public $biji;
+    public $warna;
+    public $umur;
+
+    public function matang()
+    {
+        return "buahnya sudah matang";
+    }
+}
---
+++ Day 12/Tumbuhan.php
@@ -0,0 +1,13 @@
+<?php
+
+class Tumbuhan
+{
+    public $akar;
+    public $daun;
+    public $batang;
+
+    public function bertumbuh()
+    {
+        return "Tumbuhan sedang bertumbuh";
+    }
+}
