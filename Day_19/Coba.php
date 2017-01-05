bfc3d5a16878af29dacd175fcb343ebfb6d3206a
Abihu Haezer <abihu.haezer@gmail.com>
2017-01-05 10:58:48 +0700

    Operator Scope Resolution

---
+++ Day_19/Coba.php
@@ -0,0 +1,10 @@
+<?php
+
+class Coba
+{
+  const KONSTANTA = 'Nilai Konstan';
+}
+
+echo MyClass::KONSTANTA."<br/>";  //menggunakan nama untuk merujuk pada class
+
+$classname = 'MyClass';  //menggunakan variabel untuk merujuk pada class
